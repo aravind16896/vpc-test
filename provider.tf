bac882fb4d57c1129ee347bdca8b489f996e0e2c
aravind <aravind16081996@gmail.com>
2024-04-16 15:57:35 +0530

    first1

--- provider.tf
+++ provider.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "daws76s"
+    bucket = "aravind-16896"
     key    = "vpc-test"
     region = "us-east-1"
-    dynamodb_table = "daws76s-locking"
+    dynamodb_table = "aravind16896-locking"
   }
 }
 
